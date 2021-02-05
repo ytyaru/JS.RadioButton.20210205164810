2957aa2a4fd5a8e251cb9a0e03712db5cb5a9307
ytyaru <yttry0@gmail.com>
2021-02-05 17:08:40 +0900

    docs

--- docs/1/main.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', (event) => {
-    let sub = new Sub();
-    sub.show();
-});
--- docs/1/sub.js
+++
@@ -1,4 +0,0 @@
-class Sub {
-    constructor() { this.msg="Hello JavaScript!!"; }
-    show() { console.log(this.msg); alert(this.msg); }
-}
