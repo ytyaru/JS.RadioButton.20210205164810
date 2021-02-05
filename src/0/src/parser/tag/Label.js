77e92188085bdcc708a64470893af39389bbdc10
ytyaru <yttry0@gmail.com>
2021-02-05 17:08:56 +0900

    remove src

--- src/0/src/parser/tag/Label.js
+++
@@ -1,8 +0,0 @@
-class Label {
-    static create(radio, option, input_id) {
-        const label = document.createElement('label');
-        label.innerHTML = option.innerHTML;
-        label.setAttribute('for', input_id);
-        return label;
-    }
-}
